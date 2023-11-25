7e19d39006106142b52cb1df33010b276cdfcebc
David Cruz <davidflres29@gmail.com>
2023-11-24 23:28:55 -0600

    toolbar,content & maps estable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { HomeComponent } from './home/home.component';
+import { ContentComponent } from './components/content/content.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component'
+import { NgImageSliderModule } from 'ng-image-slider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContentComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatMenuModule,
+    NgImageSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
